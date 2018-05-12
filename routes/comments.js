712cb50419fa81176f3eaabb51d95c249015304f
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-12 21:28:51 +0100

    Add Comment edit/update/delete routes/view

--- routes/comments.js
+++ routes/comments.js
@@ -42,12 +42,67 @@ router.post('/', ensureAuthenticated, (req, res) => {
   });
 });
 
-// middleware to check if user is authenticated
+// Edit
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, comment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {campgroundId: req.params.id, comment: comment});
+    }
+  });
+});
+
+// Update
+router.put('/:comment_id', (req, res) => {
+  // TODO: sanitize possible html input?
+  // req.body.campground.description = req.sanitize(req.body.campground.description);
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, comment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
+// Destroy
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, err => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
+// middleware functions
 function ensureAuthenticated (req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
+}
+
+function ensureCommentAuthor (req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, (err, campground) => {
+      if (err) {
+        res.redirect('back');
+      } else {
+        // is user the campground's author?
+        if (campground.author.id.equals(req.user._id)) {
+          return next();
+        } else {
+          res.send('No permission!');
+        }
+      }
+    });
+  // not authenticated
+  } else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
