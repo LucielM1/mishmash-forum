875ef9da6e8864e83856ba3be97fb8378551b1dd
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-17 11:10:00 +0100

    Add close button to flash messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,34 @@
 $(document).ready(function () {
-  setTimeout(function () {
+  // flash-messages close btn
+  $(".flash-messages a").click(function () {
     $(".flash-messages").fadeOut("slow");
-  }, 3000);
-  // TODO: test adding button to flash-messages div
-  // $('#close_btn').click(function(){ $(".info").fadeOut("slow"); });
-
-  // Set jQuery.validate settings for bootstrap integration
-  jQuery.validator.setDefaults({
-    highlight: function (element) {
-      jQuery(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function (element) {
-      jQuery(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
-    errorClass: 'label label-danger',
-    errorPlacement: function (error, element) {
-      if (element.parent('.input-group').length) {
-        error.insertAfter(element.parent());
-      } else {
-        error.insertAfter(element);
-      }
-    }
   });
 
+// ==================== jquery-validation ==================== //
+// to use require_from_group https://jqueryvalidation.org/require_from_group-method/
+// TODO: revive if/when alternative external image url implemented
+// ==================== jquery-validation ==================== //
+
+  // Set jQuery.validate settings for bootstrap integration
+  // jQuery.validator.setDefaults({
+  //   highlight: function (element) {
+  //     jQuery(element).closest('.form-group').addClass('has-error');
+  //   },
+  //   unhighlight: function (element) {
+  //     jQuery(element).closest('.form-group').removeClass('has-error');
+  //   },
+  //   errorElement: 'span',
+  //   errorClass: 'label label-danger',
+  //   errorPlacement: function (error, element) {
+  //     if (element.parent('.input-group').length) {
+  //       error.insertAfter(element.parent());
+  //     } else {
+  //       error.insertAfter(element);
+  //     }
+  //   }
+  // });
+
   // custom rule to ensure that either image url or image file are selected
-  // TODO: revive if/when alternative external image url is implemented
   // $("#new-campground-form").validate({
   //   rules: {
   //     imageLocal: {
